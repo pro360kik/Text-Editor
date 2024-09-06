e3ce8afa786cfd059f13e93c80559ad280371bf6
bastidea <alexisbastide@etu.umontpellier.fr>
2024-09-06 16:19:50 +0200

    exercice 3

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,24 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 4) {
+            System.err.println("Format attendu : remplacer;debut;fin;chaine");
+            return;
+        }
+        int debut = Integer.parseInt(parameters[1]);
+        int fin = Integer.parseInt(parameters[2]);
+        String chaine = parameters[3];
+
+        this.document.remplacer(debut, fin, chaine);
+
+        super.executer();
+    }
+}
