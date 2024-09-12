b0060e206f75b3fbd141e36d92fc9812cb305e9b
bastidea <alexisbastide@etu.umontpellier.fr>
2024-09-12 09:39:00 +0200

    ajout documentation dans Document.java 3ème fois

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -1,11 +1,11 @@
 package fr.iut.editeur.document;
 
-/*
+/**
 * Classe contenant le texte courant ainsi que les méthodes permetant d'altérer le texte courant
 */
 public class Document {
 
-    /*
+    /**
     attribut contenant le texte courant
     */
     private String texte;
@@ -14,7 +14,7 @@ public class Document {
         this.texte = "";
     }
 
-    /*
+    /**
     * méthode permetant d'obtenir le texte courant
     * @return renvoie le contenu de la variable privée texte
     */
@@ -22,7 +22,7 @@ public class Document {
         return texte;
     }
 
-    /*
+    /**
     * méthode permetant de remplacer le texte courant par un autre
     * @param texte nouveau texte a mettre
     */
@@ -30,7 +30,7 @@ public class Document {
         this.texte = texte;
     }
 
-    /*
+    /**
     * méthode permetant d'ajouter du texte à la fin du texte courant
     * @param texte nouveau texte à ajouter
     */
@@ -38,7 +38,7 @@ public class Document {
         this.texte += texte;
     }
 
-    /*
+    /**
     * méthode permetant de remplacer une portion de texte du texte courant par un autre
     * @param debut index du début de la portion à remplacer dans le texte courant
     * @param fin index de fin de la portion à remplacer dans le texte courant
@@ -50,7 +50,7 @@ public class Document {
         texte = partieGauche + remplacement + partieDroite;
     }
 
-    /*
+    /**
      * méthode permetant de mettre en majuscules une portion de texte du texte courant
      * @param debut index du début de la portion à mettre en majuscules dans le texte courant
      * @param fin index de fin de la portion à mettre en majuscules dans le texte courant
@@ -59,7 +59,7 @@ public class Document {
         remplacer(debut, fin , texte.substring(debut, fin + 1).toUpperCase());
     }
 
-    /*
+    /**
      * méthode permetant d'effacer une portion de texte du texte courant
      * @param debut index du début de la portion à effacer dans le texte courant
      * @param fin index de fin de la portion à effacer dans le texte courant
@@ -68,14 +68,14 @@ public class Document {
         remplacer(debut, fin , "");
     }
 
-    /*
+    /**
      * méthode permetant d'effacer complètement le texte courant
      */
     public void clear () {
         texte = "";
     }
 
-    /*
+    /**
     * méthode permetant d'afficher tous les attributs de Document en string dans la sortie standard
     * @return le string final
     */
