5c658de5a85723b153396ba48cb5545fc72545df
bastidea <alexisbastide@etu.umontpellier.fr>
2024-09-12 09:25:49 +0200

    ajout documentation dans Document.java

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -7,33 +7,64 @@ public class Document {
     public Document() {
         this.texte = "";
     }
-	
+
+    /*
+    * méthode permetant d'obtenir le texte courant
+    * @return renvoie le contenu de la variable privée texte
+    */
     public String getTexte() {
         return texte;
     }
 
+    /*
+    * méthode permetant de remplacer le texte courant par un autre
+    * @param texte nouveau texte a mettre
+    */
     public void setTexte(String texte) {
         this.texte = texte;
     }
 
+    /*
+    * méthode permetant d'ajouter du texte à la fin du texte courant
+    * @param texte nouveau texte à ajouter
+    */
     public void ajouter(String texte) {
         this.texte += texte;
     }
 
+    /*
+    * méthode permetant de remplacer une portion de texte du texte courant par un autre
+    * @param debut index du début de la portion à remplacer dans le texte courant
+    * @param fin index de fin de la portion à remplacer dans le texte courant
+    * @param remplacement texte qui va remplacer la portion donnée du texte courant
+    */
     public void remplacer (int debut, int fin, String remplacement) {
         String partieGauche = texte.substring(0, debut);
         String partieDroite = texte.substring(fin + 1);
         texte = partieGauche + remplacement + partieDroite;
     }
 
+    /*
+     * méthode permetant de mettre en majuscules une portion de texte du texte courant
+     * @param debut index du début de la portion à mettre en majuscules dans le texte courant
+     * @param fin index de fin de la portion à mettre en majuscules dans le texte courant
+     */
     public void majuscules (int debut, int fin) {
         remplacer(debut, fin , texte.substring(debut, fin + 1).toUpperCase());
     }
 
+    /*
+     * méthode permetant d'effacer une portion de texte du texte courant
+     * @param debut index du début de la portion à effacer dans le texte courant
+     * @param fin index de fin de la portion à effacer dans le texte courant
+     */
     public void effacer (int debut, int fin) {
         remplacer(debut, fin , "");
     }
 
+    /*
+     * méthode permetant d'effacer complètement le texte courant
+     */
     public void clear () {
         texte = "";
     }
